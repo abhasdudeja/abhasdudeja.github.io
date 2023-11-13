c1fcd78d1b18543b1fcc9a0ede4708ed438dfdbc
Abhas Dudeja <amdudeja@gmail.com>
2023-11-13 22:16:39 +0530

    TMM pushed

---
+++ tmm/assets/js/script.js
@@ -0,0 +1,7 @@
+var map = L.map('leaflet-map').setView([23.14,70.55],5);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
