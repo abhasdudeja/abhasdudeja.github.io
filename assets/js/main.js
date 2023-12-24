3bc25c19ff9d1beff718808f0d85b279f4707270
Abhas Dudeja <amdudeja@gmail.com>
2023-12-24 19:09:44 +0530

    try

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,9 +10,9 @@ function createNavbar() {
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
                         <li class="nav-item mx-0 mx-lg-1"><a class="nav-link active py-3 px-0 px-lg-3 rounded" href="index.html#about">About</a></li>
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link active py-3 px-0 px-lg-3 rounded" href="index.html#proficiency-skills">Proficiency &amp; Skills</a></li>
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link active py-3 px-0 px-lg-3 rounded" href="projects.html">Projects</a></li>
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link active py-3 px-0 px-lg-3 rounded" href="index.html#footer">Contact Me</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="index.html#proficiency-skills">Proficiency &amp; Skills</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="projects.html">Projects</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="index.html#footer">Contact Me</a></li>
                     </ul>
                 </div>
             </div>
